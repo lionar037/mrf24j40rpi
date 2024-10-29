9b5156a198547733451764253c5e0887cedd3644
lionar037 <8897224+lionar037@users.noreply.github.com>
2024-10-28 21:39:02 -0300

    update 202410282139

--- include/tyme/tyme.hpp
+++ include/tyme/tyme.hpp
@@ -10,8 +10,8 @@ struct Time_t
     ~Time_t();
     void delay_ms(const uint32_t);
     const std::string get_tyme();
-    private:
-        uint32_t m_delay{0};
+    //private:
+        //uint32_t m_delay{0};
 };
 
 
