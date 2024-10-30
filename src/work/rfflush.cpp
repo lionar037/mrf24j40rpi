3dc6260f614bb9090b60c8fd6d7f516c37edde62
lionar037 <8897224+lionar037@users.noreply.github.com>
2024-10-30 16:00:16 -0300

    update 202410301600

--- src/work/rfflush.cpp
+++ src/work/rfflush.cpp
@@ -49,9 +49,10 @@ namespace FFLUSH{
     { 
     system("clear");
     ///std::cout << "\033[" << 0 << ";" << 0 << std::flush;
-
+int col =0;
         for(const auto& msj : message){
-            std::cout<< msj<<"\n";
+            //std::cout<< msj<<"\n";
+            std::cout << "\033[" << col << ";" << 0 << "H"<<msj;
         }
     }
  
