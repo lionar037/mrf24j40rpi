1d51c98fc9ac8eb1583f1fb3ad922c9fdf36859c
lionar037 <8897224+lionar037@users.noreply.github.com>
2024-10-30 18:03:09 -0300

    update 202410301803

--- include/work/data_analisis.hpp
+++ include/work/data_analisis.hpp
@@ -13,13 +13,6 @@ namespace DATA{
 
     typedef struct packet_rx
     {
-        //uint32_t mac_msb;
-        //uint32_t mac_lsb;
-        //uint8_t ignore[4];
-        //uint8_t head;
-        //uint16_t size;
-        //uint8_t data[107];
-        //uint16_t checksum;
 
         uint8_t ignore[4];
         uint16_t panid;                         
@@ -31,14 +24,15 @@ namespace DATA{
         uint8_t data[107];                
         uint16_t size;
         uint16_t checksum;
+
     }PACKET_RX;
 
 
     typedef struct packet_tx
         {
-            uint8_t head;
-            uint16_t size;
+            uint8_t head;            
             char data[107];
+            uint16_t size;
             uint16_t checksum;
         }PACKET_TX;
 
