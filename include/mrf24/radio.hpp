cd73614bf63c5f4ed374cb600c350f27e6e05c2d
lionar037 <8897224+lionar037@users.noreply.github.com>
2024-10-30 02:25:26 -0300

    update 202410300225

--- include/mrf24/radio.hpp
+++ include/mrf24/radio.hpp
@@ -58,9 +58,8 @@ namespace MRF24J40{
             
         #ifdef ENABLE_INTERRUPT_MRF24 // rx
             std::unique_ptr<DATABASE::Database_t>   database{};
-            //std::unique_ptr<WORK::Work_t>           fs{};
-            
-            //struct DATA::PACKET_TX packet_data_tmp;
+            //std::unique_ptr<WORK::Work_t>           fs{};                        
+            struct DATA::packet_tx                  buffer_transmiter{};
         #else    
             std::unique_ptr<WORK::Work_t> qr{};
             struct DATA::packet_tx                  buffer_transmiter{};
