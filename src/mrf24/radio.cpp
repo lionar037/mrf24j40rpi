5b75979eb5fcec10bf1f56e0248bfd75ff77fe59
lionar037 <8897224+lionar037@users.noreply.github.com>
2024-10-30 01:09:28 -0300

    update 202410300109

--- src/mrf24/radio.cpp
+++ src/mrf24/radio.cpp
@@ -51,7 +51,7 @@ namespace MRF24J40{
         #endif
         zigbee = std::make_unique<Mrf24j>();
         
-        zigbee->init();
+        zigbee->mrf24j40_init(); //init();
 
         zigbee->interrupt_handler();
         zigbee->set_pan(PAN_ID);
