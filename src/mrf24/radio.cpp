27cec8d6f7162cd4f4bf4412648bf0e82ade1130
lionar037 <8897224+lionar037@users.noreply.github.com>
2024-10-30 20:08:59 -0300

    update 202410302008

--- src/mrf24/radio.cpp
+++ src/mrf24/radio.cpp
@@ -131,7 +131,7 @@ namespace MRF24J40{
             buffer_transmiter.size = static_cast<uint16_t>(buff.size()) + sizeof(buffer_transmiter.head) + sizeof(buffer_transmiter.checksum) ;
             std::cout<<"\n strlen(MSJ) + strlen(head) + strlen(checksum) : ( "<< std::to_string(buffer_transmiter.size) << " ) , budeffer size : ( " << std::to_string(buff.size())  <<" )\n";    
             
-            auto checksum = calculate_crc32((const std::string)buffer_transmiter.head);
+            auto checksum = calculate_crc32(reinterpret_cast<const char*>(buffer_transmiter.head));
 
             checksum += (calculate_crc32 (buff.data(), buff.size()) & 0xffffffff); 
             checksum += (buffer_transmiter.size & 0xffffffff);
