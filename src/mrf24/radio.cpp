a774b39987ef6957f08974f6bfcdf89c8a0ba5a0
lionar037 <8897224+lionar037@users.noreply.github.com>
2024-10-30 03:29:49 -0300

    update 202410300329

--- src/mrf24/radio.cpp
+++ src/mrf24/radio.cpp
@@ -270,7 +270,9 @@ namespace MRF24J40{
 
         #ifdef DBG_PRINT_GET_INFO                     
           std::memcpy (  &buffer_receiver , zigbee->get_rxbuf() , sizeof(DATA::packet_rx));
-        const uint64_t address_rx_tmp = static_cast<uint64_t> (buffer_receiver.mac_msb << 32 | buffer_receiver.mac_lsb );
+        //const uint64_t address_rx_tmp = static_cast<uint64_t> (buffer_receiver.mac_msb << 32 | buffer_receiver.mac_lsb );
+        const uint64_t address_rx_tmp = (static_cast<uint64_t>(buffer_receiver.mac_msb) << 32) | buffer_receiver.mac_lsb;
+
         if(ADDRESS_LONG_SLAVE == address_rx_tmp){ std::cout<< "\nmac es igual\n"; }
         else { std::cout<< "\nmac no es igual\n" ; }
             std::cout<< "\ndata_receiver->mac : " ; print_to_hex(address_rx_tmp ); 
