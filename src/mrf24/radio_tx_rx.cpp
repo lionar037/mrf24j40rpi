4b872ea050e860a2edf6ae0091fcce4ecf919b80
lionar037 <8897224+lionar037@users.noreply.github.com>
2024-11-01 19:33:48 -0300

    update 202411011933

--- src/mrf24/radio_tx_rx.cpp
+++ src/mrf24/radio_tx_rx.cpp
@@ -18,7 +18,7 @@
 
 namespace MRF24J40{
     extern std::unique_ptr<Mrf24j> zigbee ;
-    extern DATA::PACKET_RX buffer_receiver{};
+    extern DATA::PACKET_RX buffer_receiver;
 }
 
 namespace MRF24J40{
