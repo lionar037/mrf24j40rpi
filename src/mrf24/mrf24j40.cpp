67773d1aa3f332f6f6a30404731776b43cb5a0f3
lionar037 <8897224+lionar037@users.noreply.github.com>
2024-11-05 21:38:38 -0300

    update 202411052138

--- src/mrf24/mrf24j40.cpp
+++ src/mrf24/mrf24j40.cpp
@@ -201,7 +201,7 @@ namespace MRF24J40{
             // printf(" frame length : %d \n",frame_length);
             // printf(" rx datalength : %d \n",rx_datalength());
 
-            for(size_t i = 0; i < frame_length ; ++i) {//original
+            for(size_t i = 0; i < 127-3 ; ++i) {//original
             //for (uint16_t i = 0; i < frame_length + rx_datalength(); i++) {
                 rx_info.rx_data[++rd_ptr] = read_long(0x301 + m_bytes_MHR + i);
             }
