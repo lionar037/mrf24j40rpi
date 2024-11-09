75cc56c2b14507acece0663709efc7f66abe7418
lionar037 <8897224+lionar037@users.noreply.github.com>
2024-11-09 02:03:11 -0300

    update 202411090203

--- src/mrf24/radio_trasnreceiver.cpp
+++ src/mrf24/radio_trasnreceiver.cpp
@@ -59,17 +59,15 @@ extern uint8_t rx_buf[A_MAX_PHY_PACKET_SIZE];
 
         oss_zigbee.str("");
         oss_zigbee.clear(); 
-        monitor->insert(" ");
+        //monitor->insert(" ");
 
         monitor->insert(" Packet data (PHY Payload) :" +  std::to_string( frame_length  ));
         monitor->insert(" ");
         if(zigbee->get_bufferPHY()){
             #ifdef DBG_PRINT_GET_INFO               
 
-            for (uint8_t i = 0; i < frame_length; ++i){
-                //oss_zigbee <<std::hex<< zigbee->get_rxbuf()[i];//imprime en hexadecimal
-                oss_zigbee << zigbee->get_rxbuf()[i];
-            }
+            for (uint8_t i = 0; i < frame_length; ++i)
+                oss_zigbee << zigbee->get_rxbuf()[i];        
 
             monitor->insert(oss_zigbee.str());
             oss_zigbee.str("");   // Limpiar el contenido
@@ -95,32 +93,28 @@ extern uint8_t rx_buf[A_MAX_PHY_PACKET_SIZE];
  
             monitor->insert(" " );
             monitor->insert(" " );
-            monitor->insert("info_zigbee : " );
-            monitor->insert( oss_zigbee.str());        
+            //monitor->insert("info_zigbee : " );
+            //monitor->insert( oss_zigbee.str());        
             oss_zigbee.str("");   // Limpiar el contenido
             oss_zigbee.clear();   // Restablecer el estado
             
-        #ifdef DBG_PRINT_GET_INFO                     
-        //std::memcpy (  &buffer_receiver , rx_buf , 127);          
-        //std::memcpy (  &buffer_receiver , zigbee->get_rxbuf() , sizeof(DATA::packet_rx));          
-        std::memcpy (  &buffer_receiver , zigbee->get_rxbuf() , sizeof(rx_buf));          
-        //std::memcpy (  &buffer_receiver , zigbee->get_rxinfo() , sizeof(DATA::packet_rx));          
+        #ifdef DBG_PRINT_GET_INFO                                     
+        std::memcpy (  &buffer_receiver , zigbee->get_rxbuf() , sizeof(rx_buf));                  
 
         const uint64_t mac_address_rx = (static_cast<uint64_t>(buffer_receiver.mac_msb_rx) << 32) | buffer_receiver.mac_lsb_rx;
         const uint64_t mac_address_tx = (static_cast<uint64_t>(buffer_receiver.mac_msb) << 32) | buffer_receiver.mac_lsb;
-            monitor->insert (" " );
-            //monitor->insert (" " );
+        monitor->insert (" " );            
             //compara la direccion de mac "slave" con la mac de "entrada"
         if(ADDRESS_LONG_SLAVE == mac_address_rx){
             monitor->insert ("mac aceptada" ); }
         else { //muestra una direcion mac diferente a la configurada
             monitor->insert ("mac no es aceptada" );}
-            monitor->insert( "rx data_receiver->mac : "         + hex_to_text( mac_address_rx )); 
-            monitor->insert( "tx data_receiver->mac : "         + hex_to_text( mac_address_tx )); 
-            monitor->insert( "buffer_receiver->head : "         + hex_to_text( buffer_receiver.head ));
+            monitor->insert( "rx data_receiver->mac : 0x"         + hex_to_text( mac_address_rx )); 
+            monitor->insert( "tx data_receiver->mac : 0x"         + hex_to_text( mac_address_tx )); 
+            monitor->insert( "buffer_receiver->head : 0x"         + hex_to_text( buffer_receiver.head ));
             monitor->insert( "buffer_receiver->size : "         + std::to_string( buffer_receiver.size )); 
-            monitor->insert( "buffer_receiver->panid : "        + hex_to_text( buffer_receiver.panid ));
-            monitor->insert( "buffer_receiver->checksum : "     + hex_to_text( buffer_receiver.crc8 ));            
+            monitor->insert( "buffer_receiver->panid : 0x"        + hex_to_text( buffer_receiver.panid ));
+            monitor->insert( "buffer_receiver->checksum : 0x"     + hex_to_text( buffer_receiver.crc8 ));            
 
             std::string txt_tmp ;
             txt_tmp.assign(reinterpret_cast<const char*>(buffer_receiver.data), sizeof(buffer_receiver.data));
@@ -129,7 +123,7 @@ extern uint8_t rx_buf[A_MAX_PHY_PACKET_SIZE];
             //obtiene la direccion de mac seteada en el mrf24j40
             uint64_t mac_address;
             zigbee->mrf24j40_get_extended_mac_addr(&mac_address);
-            monitor->insert("get address mac : "               + hex_to_text(mac_address));            
+            monitor->insert("get address mac : 0x"               + hex_to_text(mac_address));            
         #endif        
             RST_COLOR() ; 
             SET_COLOR(SET_COLOR_CYAN_TEXT);
@@ -138,7 +132,7 @@ extern uint8_t rx_buf[A_MAX_PHY_PACKET_SIZE];
             monitor->insert("Frame Length : " + std::to_string(zigbee->get_rxinfo()->frame_length) );
             
             monitor->insert( "sizeof - buffer_receiverRX : "  +  std::to_string(sizeof(buffer_receiver) ) );            
-            monitor->insert( "sizeof - buffer_receiverRX.data : "  +  std::to_string(sizeof(buffer_receiver.data) ) );
+            //monitor->insert( "sizeof - buffer_receiverRX.data : "  +  std::to_string(sizeof(buffer_receiver.data) ) );
             std::string rx_b(rx_buf, rx_buf + sizeof(rx_buf));            
             //monitor->insert( "ZigeBee : "     +    rx_b  );
         //imprime todo los datos obtenidos
