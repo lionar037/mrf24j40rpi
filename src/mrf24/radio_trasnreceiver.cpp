fb34a242ac7a64f4c2d0128fb97262ab0fe769e2
lionar037 <8897224+lionar037@users.noreply.github.com>
2024-11-09 02:13:01 -0300

    update 202411090213

--- src/mrf24/radio_trasnreceiver.cpp
+++ src/mrf24/radio_trasnreceiver.cpp
@@ -100,8 +100,8 @@ extern uint8_t rx_buf[A_MAX_PHY_PACKET_SIZE];
             
         #ifdef DBG_PRINT_GET_INFO                                     
         //std::memcpy (  &buffer_receiver , zigbee->get_rxbuf() , sizeof(rx_buf));                  
-        
-        std::memcpy (  &buffer_receiver , zigbee->get_rxbuf() , sizeof(DATA::packet_tx));                  
+
+        std::memcpy (  &buffer_receiver , zigbee->get_rxbuf() , zigbee->rx_datalength());                  
 
         const uint64_t mac_address_rx = (static_cast<uint64_t>(buffer_receiver.mac_msb_rx) << 32) | buffer_receiver.mac_lsb_rx;
         const uint64_t mac_address_tx = (static_cast<uint64_t>(buffer_receiver.mac_msb) << 32) | buffer_receiver.mac_lsb;
