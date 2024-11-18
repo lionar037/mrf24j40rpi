548b56c343813f1b113d039351279a945d4c1344
lionar037 <8897224+lionar037@users.noreply.github.com>
2024-11-18 03:02:15 -0300

    update 202411180302

--- scripts/git_commit.sh
+++ scripts/git_commit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=version1.3
+VERSION=version1.4
 
 # Obtener la fecha y hora actual en el formato YYYYMMDDHHMM
 timestamp=$(date +'%Y%m%d%H%M')
@@ -9,10 +9,7 @@ timestamp=$(date +'%Y%m%d%H%M')
 commit_message="update $timestamp"
 echo "NAME GIT :"
 echo ${commit_message}
+
 # Ejecutar los comandos git
-#git add . && git commit -m "$commit_message" && git push -u origin main
 git add . && git commit -m "$commit_message" && git push -u origin $VERSION
 
-#git branch --set-upstream-to=origin/main $VERSION
-#git push origin nueva_rama:nueva_rama_remota
-#git push origin version1.1:version1.1
--- src/gpio/gpio.cpp
+++ src/gpio/gpio.cpp
@@ -83,7 +83,6 @@ extern "C"{
     #endif
             std::this_thread::sleep_for(std::chrono::milliseconds(50));
         }
-
         return false;
     }
 
